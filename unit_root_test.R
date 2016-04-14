f317be8e1f3057b37943bdaa36fb07819449f95f
z080328 <zhouqiao_ntu@yahoo.com>
2016-04-14 10:44:29 +0800

    Added data for t-bills

--- unit_root_test.R
+++ unit_root_test.R
@@ -1,3 +1,20 @@
 E=rnorm(240)
 X=cumsum(E)
 plot(X,type="l")
+
+lags=0
+z=diff(X)
+n=length(z)
+z.diff=embed(z, lags+1)[,1]
+z.lag.1=X[(lags+1):n]
+summary(lm(z.diff~0+z.lag.1 ))
+
+summary(lm(z.diff~0+z.lag.1 ))$coefficients
+
+library(urca)
+df=ur.df(X,type="none",lags=0)
+df
+
+qnorm(c(.01,.05,.1)/2)
+
+summary(df)
